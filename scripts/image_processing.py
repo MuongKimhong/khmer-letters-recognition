0a0048516c1d4714459118d7f4239171fa243eca
MuongKimhong <muongkimhong086@gmail.com>
2020-10-30 11:56:31 +0700

    update ResizeImage's constructor and crop method

--- scripts/image_processing.py
+++ scripts/image_processing.py
@@ -1,4 +1,5 @@
 import os
+from dataclasses import dataclass
 import cv2
 from imutils import paths
 import numpy as np
@@ -45,19 +46,26 @@ How to use ResizeImage:
 
 4. For new_name, if rename only 1 image then new file name will be the same as new_name provided but if rename more than
    one image, the new file names will be new_name1.jpg, new_name2.jpg, new_name3.jpg, ...
-'''
-class ResizeImage:
-    def __init__(self, input_image_paths, output_image_paths, size, new_name, file_type="jpg", padding_color=0):
-        self.input_image_paths = input_image_paths
-        self.output_image_paths = output_image_paths
-        self.padding_color = padding_color
-        self.new_name = new_name
-        self.file_type = file_type
-        self.size = size
 
-    def crop(self, image, top, bottom, left, right):
+5. if crop_image = True, need to provide values for top, bottom, left, right, default is False
+'''
+@dataclass(init=True)
+class ResizeImage:
+    input_image_paths: str
+    output_image_paths: str
+    size: list
+    new_name: str
+    crop_image: bool = False
+    top: int = None
+    bottom: int = None
+    left: int = None
+    right: int = None
+    file_type: str = "jpg"
+    padding_color: int = 0
+
+    def crop(self, image):
         image_height, image_width = image.shape[:2]
-        cropped_image = image[top:(image_height - bottom), left:(image_width - right)]
+        cropped_image = image[self.top:(image_height - self.bottom), self.left:(image_width - self.right)]
         return cropped_image
         
     def resize(self):
@@ -68,24 +76,45 @@ class ResizeImage:
             image_path = cv2.imread(image_path)
             height, width = image_path.shape[:2]
             new_height, new_width = self.size
-            print("[INFO] cropping image {} ....".format(i+1))
-            cropped_image = self.crop(image_path, 500, 500, 200, 200)
-            cropped_image_height, cropped_image_width = cropped_image.shape[:2]
-            # Interpolation method
-            if (cropped_image_height > new_height) or (cropped_image_width > new_width):
-                interpolation = cv2.INTER_AREA
+            if self.crop_image and ((self.top is None) or (self.bottom is None) or (self.left is None) or (self.right is None)):
+                error = "crop_image is True but top or bottom or left or right value is not provided"
+                exeption = print(error)
+                return exeption
+            if self.crop_image and (self.top is not None) and (self.bottom is not None) and (self.left is not None) and (self.right is not None):
+                print("[INFO] cropping image {} ....".format(i+1))
+                cropped_image = self.crop(image_path)
+                cropped_image_height, cropped_image_width = cropped_image.shape[:2]
+                # Interpolation method
+                if (cropped_image_height > new_height) or (cropped_image_width > new_width):
+                    interpolation = cv2.INTER_AREA
+                else:
+                    interpolation = cv2.INTER_CUBIC
+                # Aspect ratio
+                aspect_ratio = cropped_image_width / cropped_image_height
+                # set padding color 
+                if len(cropped_image.shape) == 3 and not isinstance(self.padding_color, (list, tuple, np.ndarray)):
+                    self.padding_color = [self.padding_color] * 3
+                # scale and pad
+                print("[INFO] resizing image {} ....".format(i+1))
+                scaled_image = cv2.resize(cropped_image, (new_width, new_height), interpolation=interpolation)
+                scaled_image = cv2.copyMakeBorder(scaled_image, padding_top, padding_bottom, padding_left, 
+                                                  padding_right, borderType=cv2.BORDER_CONSTANT, value=self.padding_color)
             else:
-                interpolation = cv2.INTER_CUBIC
-            # Aspect ratio
-            aspect_ratio = cropped_image_width / cropped_image_height
-            # set padding color 
-            if len(cropped_image.shape) == 3 and not isinstance(self.padding_color, (list, tuple, np.ndarray)):
-                self.padding_color = [self.padding_color] * 3
-            # scale and pad
-            print("[INFO] resizing image {} ....".format(i+1))
-            scaled_image = cv2.resize(cropped_image, (new_width, new_height), interpolation=interpolation)
-            scaled_image = cv2.copyMakeBorder(scaled_image, padding_top, padding_bottom, padding_left, 
-                                              padding_right, borderType=cv2.BORDER_CONSTANT, value=self.padding_color)
+                # Interpolation method
+                if (height > new_height) or (width > new_width):
+                    interpolation = cv2.INTER_AREA
+                else:
+                    interpolation = cv2.INTER_CUBIC
+                # Aspect ratio
+                aspect_ratio = width / height
+                # set padding color
+                if len(image_path.shape) == 3 and not isinstance(self.padding_color, (list, tuple, np.ndarray)):
+                    self.padding_color = [self.padding_color] * 3
+                # scale and pad
+                print("[INFO] resizing image {} ....".format(i+1))
+                scaled_image = cv2.resize(image_path, (new_width, new_height), interpolation=interpolation)
+                scaled_image = cv2.copyMakeBorder(scaled_image, padding_top, padding_bottom, padding_left,
+                                                  padding_right, borderType=cv2.BORDER_CONSTANT, value=self.padding_color)
             if len(image_paths) == 1:
                 output_paths = self.output_image_paths + f"/{self.new_name}" + f".{self.file_type}"
             elif len(image_paths) > 1:
