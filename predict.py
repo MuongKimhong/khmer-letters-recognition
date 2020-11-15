e1e5c59505c8c3a5e76b8c4b833a604616049dc1
MuongKimhong <muongkimhong086@gmail.com>
2020-11-15 14:20:51 +0700

    remove hand_train.py and implemented red spot detection

--- predict.py
+++ predict.py
@@ -1,16 +1,33 @@
-import numpy as np
 import cv2
+import numpy as np
 
-capture_frame = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)
+
+
+def find_red_color(hsv_frame, original_frame):
+    low_red = np.array([161, 155, 84])
+    high_red = np.array([179, 255, 255])
+    red_mask = cv2.inRange(hsv_frame, low_red, high_red)
+    red = cv2.bitwise_and(frame, frame, mask=red_mask)
+    red_spot = {'red': red, 'red_mask': red_mask}
+    return red_spot
+
 
 while True:
-    ret, frame = capture_frame.read()
+    ret, frame = capture.read()
     frame = cv2.flip(frame, 1)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    red = find_red_color(hsv_frame, frame)
+    coordinate = cv2.findNonZero(red['red_mask'])
+    if coordinate is not None:
+        print("Coordinate (x:{}, y:{})".format(coordinate[0][0][0], coordinate[0][0][1]))
+        frame = cv2.circle(frame, (coordinate[0][0][0], coordinate[0][0][1]), radius=10, color=(0, 0, 255), thickness=-1)
 
-    cv2.imshow("Frame", frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow('red', red['red'])
+    cv2.imshow('original', frame)
+    
+    key = cv2.waitKey(1)
+    if key == 27:
         break
 
 cv2.destroyAllWindows()
